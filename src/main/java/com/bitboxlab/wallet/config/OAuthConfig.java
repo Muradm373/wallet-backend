27fc4891d0412338ad21ebddc765d7f0aab7030c
Murad Mammadov <muradm373@gmail.com>
2020-11-05 12:30:40 +0200

    Authorization header updated

--- src/main/java/com/bitboxlab/wallet/config/OAuthConfig.java
+++ src/main/java/com/bitboxlab/wallet/config/OAuthConfig.java
@@ -47,10 +47,11 @@ public class OAuthConfig extends UsernamePasswordAuthenticationFilter {
                 .signWith(SignatureAlgorithm.HS512, "SecretKeyToGenJWTs".getBytes())
                 .compact();
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods",
                 "ACL, CANCELUPLOAD, CHECKIN, CHECKOUT, COPY, DELETE, GET, HEAD, LOCK, MKCALENDAR, MKCOL, MOVE, OPTIONS, POST, PROPFIND, PROPPATCH, PUT, REPORT, SEARCH, UNCHECKOUT, UNLOCK, UPDATE, VERSION-CONTROL");
         response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, content-type");
+        response.setHeader("Access-Control-Expose-Headers", "authorization");
         response.addHeader("Authorization","Bearer " + token);
     }
 
