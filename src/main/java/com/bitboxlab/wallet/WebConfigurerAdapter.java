1c4e2e1584f2daa10f1ef4c25b78c3f8537cc5a7
Murad Mammadov <muradm373@gmail.com>
2020-11-27 11:02:17 +0200

    Added swagger to the application

--- src/main/java/com/bitboxlab/wallet/WebConfigurerAdapter.java
+++ src/main/java/com/bitboxlab/wallet/WebConfigurerAdapter.java
@@ -3,6 +3,7 @@ package com.bitboxlab.wallet;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -12,4 +13,12 @@ public class WebConfigurerAdapter extends WebMvcConfigurerAdapter {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
     }
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("swagger-ui.html")
+//                .addResourceLocations("classpath:/META-INF/resources/");
+//
+//        registry.addResourceHandler("/webjars/**")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+//    }
 }
